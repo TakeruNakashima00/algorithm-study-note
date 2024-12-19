76a2904520ec46dd5919496b18f94d4951557cdb
TakeruNakashima00 <takeru.nakashima00@gmail.com>
2024-12-19 23:53:11 +0900

    [TwoPointers]ValidPalindrome

--- TwoPointers/ValidPalindrome.py
+++ TwoPointers/ValidPalindrome.py
@@ -1,9 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        print(s)
-        print(s[::-1])
+
+        newStr = ""
+        for i in s:
+            if i.isalnum():
+                newStr += i.lower()
+                print(newStr)
+        return newStr == newStr[::-1]
 
 
 if __name__ == "__main__":
     sol = Solution()
     sol.isPalindrome("Was it a car or a cat I saw?")
+
+# Strategy 1
+# 1. explicit not apnum value from given strings
+# 2. compare explicit str value and reverse str value
