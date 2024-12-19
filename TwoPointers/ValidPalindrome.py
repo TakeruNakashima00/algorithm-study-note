570640c509c55f34dd35f23aa450c8fb82d2fff9
TakeruNakashima00 <takeru.nakashima00@gmail.com>
2024-12-19 23:24:35 +0900

    [TwoPointers]ValidPalindrome

---
+++ TwoPointers/ValidPalindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        print(s)
+        print(s[::-1])
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    sol.isPalindrome("Was it a car or a cat I saw?")
