3c18b7b34c5138020ca34b1aeca3095c12528446
TakeruNakashima00 <takeru.nakashima00@gmail.com>
2024-12-23 07:54:46 +0900

    [Python-Basics] not

---
+++ Python-Basics/Not.py
@@ -0,0 +1,17 @@
+# bool
+result = False
+print(not result) #True
+
+# List
+empty_stack = []
+print(not empty_stack) #True
+
+empty_string_stack = []
+print(not empty_stack) #True
+
+# dict
+empty_dict = {}
+print(not empty_dict) #True
+
+empty_string_dict = {""}
+print(not empty_string_dict) #False
