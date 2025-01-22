653f9763fe6818f0e6cab7f9ee829340370c6dd7
TakeruNakashima00 <takeru.nakashima00@gmail.com>
2025-01-23 07:14:31 +0900

    [BinaryTree] invertBinaryTree DFS

--- BinaryTree/InvertBinaryTree.py
+++ BinaryTree/InvertBinaryTree.py
@@ -41,14 +41,14 @@ class BinarySearchTree():
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
-        queue = deque([root])
-        while queue:
-            node = queue.popleft()
-            node.left, node.right = node.right, node.left
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
+
+        # swap the children
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
 
 
