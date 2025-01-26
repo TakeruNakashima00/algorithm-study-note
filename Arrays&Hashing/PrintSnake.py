06fdc2a3f3f6750e73731761a5b364a86f5329d7
TakeruNakashima00 <takeru.nakashima00@gmail.com>
2025-01-26 16:10:33 +0900

    [List] snake_string_v2

--- Arrays&Hashing/PrintSnake.py
+++ Arrays&Hashing/PrintSnake.py
@@ -1,3 +1,4 @@
+import operator
 from typing import List
 
 
@@ -20,11 +21,34 @@ def snake_string_v1(chars: str) -> List[List[str]]:
             result[result_index].append(' ')
     return result
 
+def snake_string_v2(chars: str, depth: int) -> List[List[int]]:
+    result = [[] for _ in range(depth)]
+    result_indexes = {i for i in range(depth)}
+    insert_index = int(depth / 2)
+
+    op = operator.neg
+    for s in chars:
+        result[insert_index].append(s)
+        for result_index in result_indexes - {insert_index}:
+            result[result_index].append(' ')
+        if insert_index <= 0:
+            op = operator.pos
+        if insert_index >= depth -1:
+            op = operator.neg
+        insert_index += op(1)
+
+    return result
+
 
 if __name__ == '__main__':
     numbers = [str(i) for _ in range(5) for i in range(10)]
     strings = ''.join(numbers)
     for line in snake_string_v1(strings):
+        print(''.join(line))
+
+    import string
+    alphabet = [s for _ in range(2) for s in string.ascii_lowercase]
+    for line in snake_string_v2(alphabet, 10):
         print(''.join(line))
 
 """
