27c501fc29aaf760c319fada40572db1f6143b3f
TakeruNakashima00 <takeru.nakashima00@gmail.com>
2025-03-23 18:59:30 +0900

    [Cipher] caesar_cipher_hack

--- Cipher/caesar_cipher.py
+++ Cipher/caesar_cipher.py
@@ -2,6 +2,8 @@
 Input: Hello World
 Output: Khoor Zruog
 """
+import string
+from typing import Generator, Tuple
 
 def caesar_cipher(text: str, shift: int) -> str:
     result = ""
@@ -18,8 +20,31 @@ def caesar_cipher(text: str, shift: int) -> str:
     return result
 
 
+def caesar_cipher_hack(text: str) -> Generator[Tuple[int, str], None, None]:
 
-print(caesar_cipher("Hello World", 3))
+    alphabet_len = ord('Z') - ord('A') + 1
+
+    for candidate_shift in range(1, alphabet_len + 1):
+        reverted = ''
+        for char in text:
+            if char.islower():
+                alphabet = string.ascii_lowercase
+            elif char.isupper():
+                alphabet = string.ascii_uppercase
+            else:
+                reverted += char
+                continue
+            index = alphabet.index(char) - candidate_shift
+
+            if index < 0:
+                index += alphabet_len
+            reverted += alphabet[index]
+        yield candidate_shift, reverted
+
+encypted_string = caesar_cipher("Hello World", 3)
+
+for candidate in caesar_cipher_hack(encypted_string):
+    print(candidate)
 
 
 """
