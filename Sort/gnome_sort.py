0fe27086c8ee3f785f19ae0d1d5f8cee1ef2fbbe
TakeruNakashima00 <takeru.nakashima00@gmail.com>
2025-03-31 19:23:10 +0900

    [Sort] gnome_sort

--- Sort/gnome_sort.py
+++ Sort/gnome_sort.py
@@ -2,16 +2,18 @@ from typing import List
 import random
 
 def gnome_sort(numbers: List[int]) -> List[int]:
-    current_pointer, len_numbers = 0, len(numbers)
+    index, len_numbers = 0, len(numbers)
 
-    while current_pointer != len_numbers-1:
+    while index < len_numbers:
 
-        if numbers[current_pointer] < numbers[current_pointer+1]:
-            current_pointer += 1
+        if index == 0:
+            index += 1
+        elif numbers[index] >= numbers[index-1]:
+            index += 1
         else:
-            numbers[current_pointer], numbers[current_pointer+1] = numbers[current_pointer+1], numbers[current_pointer]
-            if current_pointer != 0:
-                current_pointer -= 1
+            numbers[index], numbers[index-1] = numbers[index-1], numbers[index]
+            index -= 1
+
     return numbers
 
 if __name__ == '__main__':
