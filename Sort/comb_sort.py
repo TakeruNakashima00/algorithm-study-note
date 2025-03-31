3fc9d539bf2d6d379492d8b54a3518ac456a2397
TakeruNakashima00 <takeru.nakashima00@gmail.com>
2025-03-31 18:41:12 +0900

    [Sort] comb_sort

---
+++ Sort/comb_sort.py
@@ -0,0 +1,35 @@
+from typing import List
+import random
+
+def comb_sort(numbers: List[int]) -> List[int]:
+
+    len_numbers = len(numbers)
+    gap = len_numbers
+    swapped = True
+
+    while gap != 1 or swapped:
+        gap = int(gap / 1.3)
+        if gap < 1:
+            gap = 1
+
+        swapped = False
+        for i in range(0, len_numbers - gap):
+            if numbers[i] > numbers[i + gap]:
+                numbers[i], numbers[i + gap] = numbers[i + gap], numbers[i]
+                swapped = True
+    return numbers
+
+
+if __name__ == '__main__':
+    numbers = [random.randint(0,1000) for _ in range(10)]
+    print(numbers)
+    numbers = comb_sort(numbers)
+    print(numbers)
+
+
+"""
+can express limit line by double for loop
+
+ for i in range(len_numbers):
+        for j in range(len_numbers - 1 - i):
+"""
